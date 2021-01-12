9363430014c7f70b30f19805777658ec20283806
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-01-12 23:38:47 +0100

    Manga DTO

--- WantList/DTO/AutoMapping.cs
+++ WantList/DTO/AutoMapping.cs
@@ -9,9 +9,13 @@ namespace WantList.DTO
         {
             // To DTO
             CreateMap<Anime, AnimeDto>();
+            CreateMap<AnidbAnime, AnidbAnimeDto>();
+            CreateMap<Manga, MangaDto>();
             
             // From DTO
             CreateMap<AnimeDto, Anime>();
+            CreateMap<AnidbAnimeDto, AnidbAnime>();
+            CreateMap<MangaDto, Manga>();
         }
     }
 }
---
+++ WantList/DTO/MangaDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WantList.DTO
+{
+    public class MangaDto
+    {
+        public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public int MangaUpdatesId { get; set; }
+        public DateTime AddedDateTime { get; set; }
+        public string Image { get; set; }
+        [Required] public int WantRank { get; set; }
+        public string MissingVolumes { get; set; }
+    }
+}
