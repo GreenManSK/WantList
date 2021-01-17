249cb96884d153d84bd052a24091323ed08b7edb
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-01-17 12:01:28 +0100

    AnidbAnime entity

---
+++ WantList.Angular/src/app/entities/anidb-anime.spec.ts
@@ -0,0 +1,7 @@
+import { AnidbAnime } from './anidb-anime';
+
+describe('AnidbAnime', () => {
+  it('should create an instance', () => {
+    expect(new AnidbAnime()).toBeTruthy();
+  });
+});
---
+++ WantList.Angular/src/app/entities/anidb-anime.ts
@@ -0,0 +1,5 @@
+export class AnidbAnime {
+  anidbId: number;
+  japanese: string;
+  english: string;
+}
