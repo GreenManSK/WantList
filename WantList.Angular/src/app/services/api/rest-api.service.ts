11cc21f2fcfda3e18a8c1f5172bdf4200a867729
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-01-21 21:35:28 +0100

    RestApi service

---
+++ WantList.Angular/src/app/services/api/rest-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClientService } from './http-client.service';
+import { environment } from '../../../environments/environment';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestApiService {
+
+  protected url: string;
+
+  constructor(public http: HttpClientService/*, private alertService: AlertService*/) {
+    this.url = environment.restUrl;
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  protected handleError<T>( operation = 'operation', result?: T ) {
+    return ( error: any ): Observable<T> => {
+
+      const msg = `${operation} failed: ${error.message}`;
+      console.error(msg, error); // log to console
+      // TODO: this.alertService.send(new AlertMessage(msg, AlertType.Error));
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
