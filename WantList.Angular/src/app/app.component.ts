2afe6f03edcaee50fc6e30fb0f7330f34374db97
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-01-22 14:50:44 +0100

    Clear cache on hard refresh

--- WantList.Angular/src/app/app.component.ts
+++ WantList.Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CacheService } from './services/cache.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'WantList';
 
-  constructor() {
+  constructor(private cacheService: CacheService) {
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent( event: KeyboardEvent ) {
+    if (event.ctrlKey && event.key === 'F5') {
+      this.cacheService.cleanLocalStorage();
+    }
   }
 }
