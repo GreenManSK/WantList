07d13f41569e551db3c0c3a2b4940640c2bef5a5
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-01-22 17:59:33 +0100

    Set movie episode count

--- WantList.Anidb/AnidbService.cs
+++ WantList.Anidb/AnidbService.cs
@@ -93,6 +93,7 @@ namespace WantList.Anidb
                 var date = publishedDateNode.First().InnerText;
                 animeData.ReleaseDate = DateTime.ParseExact(date, "dd.MM.yyyy", null);
                 animeData.Type = AnimeType.Movie;
+                animeData.EpisodeCount = 1;
             }
 
             return animeData;
