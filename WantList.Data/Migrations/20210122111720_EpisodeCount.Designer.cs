edae20b6dd1563b6eb9c27532730545528a768ec
Lukáš Kurčík <lukas.kurcik@gmail.com>
2021-01-22 12:17:53 +0100

    Episode count migration

--- WantList.Data/Migrations/20210122111720_EpisodeCount.Designer.cs
+++ WantList.Data/Migrations/20210122111720_EpisodeCount.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WantList.Data;
 
 namespace WantList.Data.Migrations
 {
     [DbContext(typeof(WantListDbContext))]
-    partial class WantListDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210122111720_EpisodeCount")]
+    partial class EpisodeCount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
